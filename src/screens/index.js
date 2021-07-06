269ebd3b08f597baab85b0b4b0e2fae945f6a8cf
Eik <etty.ikpeme@gmail.com>
2021-07-06 13:53:45 -0500

    update

--- src/screens/index.js
+++ src/screens/index.js
@@ -5,3 +5,4 @@ export { default as HomeScreen } from './HomeScreen/HomeScreen'
 export { default as RegistrationScreen } from './RegistrationScreen/RegistrationScreen'
 
 export { default as ProvideScreen } from './ProvideScreen/'
+
