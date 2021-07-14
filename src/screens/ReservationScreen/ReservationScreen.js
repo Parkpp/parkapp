890338f427feaba15bd80b4e88af77d44e955857
Sherman Tung <shermantung@outlook.com>
2021-07-14 15:05:34 -0400

    Reworked reservation page to have time selectors for both ios and android

--- src/screens/ReservationScreen/ReservationScreen.js
+++ src/screens/ReservationScreen/ReservationScreen.js
@@ -4,144 +4,203 @@ import {
   Text,
   View,
   TouchableOpacity,
-  StatusBar,
+  SafeAreaView,
+  ScrollView,
+  Platform,
 } from "react-native";
 import styles from "./styles";
 import { firebase } from "../../firebase/config";
-
-//Use a duration to select time
-
-// .1 A list of times  
-// .2 Modify the list to contain only times within start and end time
-    //
-//
-
-
-
-const mockData = {
-  slots: {
-    slot1: "9:00am to 9:30am",
-    slot2: "9:30am to 10:00am",
-    slot3: "10:00am to 10:30am",
-    slot4: "10:30am to 11:00am",
-    slot5: "11:00am to 11:30am",
-    slot6: "11:30am to 12:00pm",
-  },
-};
-
-const mockDataArr = [
-  "9:00am to 9:30am",
-  "9:30am to 10:00am",
-  "10:00am to 10:30am",
-  "10:30am to 11:00am",
-  "11:00am to 11:30am",
-  "11:30am to 12:00pm",
-];
+import DateTimePicker from "@react-native-community/datetimepicker";
 
 export default function ReservationScreen(props) {
   const user = props.route.params.user;
-  const spot = props.route.params.spot
+  const spot = props.route.params.spot;
 
-  const startTimeLimit = spot.startTime
-  const endTimeLimit = spot.endTinme
+  // const startTimeLimit = spot.startTime;
+  // const endTimeLimit = spot.endTinme;
 
-  const [vehicle, setVehicle] = useState({})
-  const [selected, setSelected] = useState([]);
+  // startTimeHour = Number(startTimeLimit.slice(0, 2)) * 60 * 60;
+  // startTimeMins = Number(startTimeLimit.slice(3, 5)) * 60;
 
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     bookingDate: this.props.navigation.state.params.bookingDate,
-  //   };
-  // }
-  // onPressBack() {
-  //   const { goBack } = this.props.navigation;
-  //   goBack();
-  // }
-  // bookSlot(status, key, value) {
-  //   const month = this.state.bookingDate.month;
-  //   const date = this.state.bookingDate.day;
-  //   const user = firebase.auth().currentUser;
-  //   const uid = user.uid;
-  //   let userDataJson = {};
-  //   if (status) userDataJson[key] = uid;
-  //   else userDataJson[key] = null;
+  // endTimeHour = Number(endTimeLimit.slice(0, 2)) * 60 * 60;
+  // endTimeMins = Number(endTimeLimit.slice(3, 5)) * 60;
 
-  // firebase
-  //   .database()
-  //   .ref("users")
-  //   .child(uid)
-  //   .child("appointments")
-  //   .child(month)
-  //   .child(date)
-  //   .update(userDataJson);
-  // }
+  // startTimeLimit = startTimeHour + startTimeMins;
+  // endTimeLimit = endTimeHour + endTimeMins;
 
-
-  // Recieve user and parking spot props from single mapspotscreen component
+  const [vehicles, setVehicles] = useState([]);
+  // const [selected, setSelected] = useState([]);
+  const [startTime, setStartTime] = useState(new Date());
+  const [endTime, setEndTime] = useState(new Date());
+  const [showStartTime, setShowStartTime] = useState(false);
+  const [showEndTime, setShowEndTime] = useState(false);
+  const [androidStartTime, setAndroidStartTime] = useState(null);
+  const [androidEndTime, setAndroidEndTime] = useState(null);
 
   //Make call to firebase to retrieve user vehicle information
+  useEffect(() => {
+    (async () => {
+      const db = firebase.firestore();
+      const vehicleRef = db
+        .collection("vehicles")
+        .where("userId", "==", user.id);
+      const snapshot = await vehicleRef.get();
+      let vehiclesData = [];
+      snapshot.forEach((doc) => {
+        vehiclesData.push(doc.data());
+      });
+      setVehicles(vehiclesData);
+      console.log(vehicles);
+    })();
+  }, []);
 
-  // useEffect(() => {
-  //   (async () => {
+  const onChangeStartTime = (event, selectedTime) => {
+    const currentTime = selectedTime || startTime;
+    setStartTime(currentTime);
+    let tempSelection = new Date(currentTime);
+    // Returns date object
+    let tempTime = tempSelection.getHours() + ":" + tempSelection.getMinutes();
+    //Returns a time string
+    console.log("Selected Start Time-->", `Start Time:${tempTime}`);
+    setAndroidStartTime(tempTime);
+    setShowStartTime(!showStartTime);
+  };
 
-  //     const db = firebase.firestore();
-  //     let vehicle = db.collection("vehicles").where("userId", "==", user.id);
+  const revealStartTimeSelector = () => {
+    setShowStartTime(true);
+  };
 
-  //      vehicle = vehicle.get()
-  //      console.log(vehicle)
-     
-  //   })();
-  // }, []);
+  const onChangeEndTime = (event, selectedTime) => {
+    const currentTime = selectedTime || endTime;
+    setEndTime(currentTime);
+    let tempSelection = new Date(currentTime);
+    // Returns date object
+    let tempTime = tempSelection.getHours() + ":" + tempSelection.getMinutes();
+    //Returns a time string
+    console.log("Selected End Time-->", `End Time:${tempTime}`);
+    setAndroidEndTime(tempTime);
+    setShowEndTime(!showEndTime);
+    //Validation occurs here so that End time > Start Time
+  };
 
+  const revealEndTimeSelector = () => {
+    setShowStartTime(true);
+  };
 
-  //Make call to firebase to 
-
- 
-  const handlePress = (slot) =>
-    selected.includes(slot)
-      ? setSelected(selected.filter((s) => s !== slot))
-      : setSelected([...selected, slot]);
-
-  const slots = mockData.slots;
-  const slotsarr = Object.keys(slots).map(function (k) {
-    return (
-      <View key={k} style={{ margin: 5 }}>
-        <TouchableOpacity
-          //onPress={(status) => this.bookSlot(status, k, slots[k])}
-          onPressIn={() => handlePress(slots[k])}
-          style={[
-            styles.button,
-            {
-              backgroundColor: selected.includes(slots[k])
-                ? "#00cc00"
-                : "#788eec",
-            },
-          ]}
-        >
-          <Text style={styles.buttonTitle}>{slots[k]}</Text>
-        </TouchableOpacity>
-      </View>
-    );
-  });
-
+  //If Platform = android, create state for buttons to show when clicked that renders out a time selection
+  console.log("What is the current start time prompt---->", startTime);
   return (
-    <View style={styles.container}>
-      <StatusBar barStyle="light-content" />
+    <SafeAreaView>
+      <ScrollView>
+        {/*Vehicle Information*/}
+        {vehicles.map((vehicle, idx) => {
+          return (
+            <View key={idx}>
+              <Text>Brand: {vehicle.make}</Text>
+              <Text>Model: {vehicle.model}</Text>
+              <Text>Year: {vehicle.year}</Text>
+              <Text>Color: {vehicle.color}</Text>
+              <Text>License Plate: {vehicle.licensePlate}</Text>
+            </View>
+          );
+        })}
 
-      <View>
-        <TouchableOpacity
-          style={[styles.button, { backgroundColor: "navy" }]}
-          onPress={() => this.onPressBack()}
-        >
-          <Text style={styles.buttonTitle}>Go Back</Text>
-        </TouchableOpacity>
-      </View>
-      <Text>Select a Time Slot!</Text>
-      {slotsarr}
-      <TouchableOpacity style={[styles.button, { backgroundColor: "navy" }]}>
-        <Text style={styles.buttonTitle}>Confirm Booking</Text>
-      </TouchableOpacity>
-    </View>
+        {Platform.OS == "ios" ? (
+          //IOS View for Time Selector
+          <View
+            style={{
+              flex: 1,
+              flexDirection: "row",
+              alignItems: "center",
+            }}
+          >
+            {/*Start Time*/}
+            <Text>Start Time:</Text>
+            <View style={{ flex: 1, paddingHorizontal: 10 }}>
+              <DateTimePicker
+                testId="Start Time"
+                value={startTime}
+                mode={"time"}
+                display="default"
+                onChange={onChangeStartTime}
+                minuteInterval={30}
+                is24hour={true}
+                style={{ margin: 10 }}
+              />
+            </View>
+            {/*End Time*/}
+
+            <Text>End Time: </Text>
+            <View style={{ flex: 1, paddingHorizontal: 10 }}>
+              <DateTimePicker
+                testId="End Time"
+                value={endTime}
+                mode={"time"}
+                display="default"
+                onChange={onChangeEndTime}
+                minuteInterval={30}
+                is24hour={true}
+                style={{ margin: 10 }}
+              />
+            </View>
+          </View>
+        ) : (
+          //Android View for Time Selector
+          <View>
+            <Text>Start Time: </Text>
+            <View>
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() => revealStartTimeSelector()}
+              >
+                {!androidStartTime ? (
+                  <Text>Select Start Time</Text>
+                ) : (
+                  <Text>{androidStartTime}</Text>
+                )}
+              </TouchableOpacity>
+            </View>
+            <Text>End Time: </Text>
+            <View>
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() => revealEndTimeSelector()}
+              >
+                {!androidEndTime ? (
+                  <Text>Select End Time</Text>
+                ) : (
+                  <Text>{androidEndTime}</Text>
+                )}
+              </TouchableOpacity>
+            </View>
+
+            {showStartTime && (
+              <DateTimePicker
+                testId="Start Time"
+                value={startTime}
+                mode={"time"}
+                display="spinner"
+                onChange={onChangeStartTime}
+                minuteInterval={30}
+                is24hour={true}
+                style={{ margin: 10 }}
+              />
+            )}
+            {showEndTime && (
+              <DateTimePicker
+                testId="End Time"
+                value={endTime}
+                mode={"time"}
+                display="spinner"
+                onChange={onChangeEndTime}
+                minuteInterval={30}
+                is24hour={true}
+                style={{ margin: 10 }}
+              />
+            )}
+          </View>
+        )}
+      </ScrollView>
+    </SafeAreaView>
   );
 }
