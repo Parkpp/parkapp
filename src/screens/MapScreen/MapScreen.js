9ec176f40d84ceef99f104ccbcc2110de165ee71
Jared Irwin <jaredirwin@Jareds-MacBook-Air.local>
2021-07-15 10:55:34 -0400

    Map colors

--- src/screens/MapScreen/MapScreen.js
+++ src/screens/MapScreen/MapScreen.js
@@ -1,17 +1,17 @@
-import React, { useEffect, useState, useRef } from 'react';
-import { Text, View, SafeAreaView } from 'react-native';
-import styles from './styles';
-import { firebase, GOOGLE_API_KEY } from '../../firebase/config';
+import React, { useEffect, useState, useRef } from "react";
+import { Text, View, SafeAreaView } from "react-native";
+import styles from "./styles";
+import { firebase, GOOGLE_API_KEY } from "../../firebase/config";
 import MapView, {
   PROVIDER_GOOGLE,
   Marker,
   Callout,
-  Camera
-} from 'react-native-maps';
-import * as Location from 'expo-location';
-import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
-import Geocoder from 'react-native-geocoding';
-import { LogBox } from 'react-native';
+  Camera,
+} from "react-native-maps";
+import * as Location from "expo-location";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import Geocoder from "react-native-geocoding";
+import { LogBox } from "react-native";
 // import { dummyData } from '../../../parkingSeed';
 // const db = firebase.firestore();
 // dummyData.forEach(doc => {
@@ -20,39 +20,39 @@ import { LogBox } from 'react-native';
 
 LogBox.ignoreAllLogs(true);
 
-Geocoder.init(GOOGLE_API_KEY, { language: 'en' });
+Geocoder.init(GOOGLE_API_KEY, { language: "en" });
 
-export default function MapScreen (props) {
+export default function MapScreen(props) {
   const [parkingSpots, setParkingSpots] = useState(null);
   const [region, setRegion] = useState({
     center: {
       latitude: 40.757952,
-      longitude: -73.985572
+      longitude: -73.985572,
     },
     pitch: 0,
     zoom: 13,
     heading: 0,
-    altitude: 0
+    altitude: 0,
   });
 
-  const geocode = async text => {
+  const geocode = async (text) => {
     let coords = await Geocoder.from(text);
     return {
       latitude: coords.results[0].geometry.location.lat,
-      longitude: coords.results[0].geometry.location.lng
+      longitude: coords.results[0].geometry.location.lng,
     };
   };
 
   let snapshot;
   const fetchParkingSpots = async () => {
     const db = firebase.firestore();
-    const parkingSpotsRef = db.collection('parkingSpots');
+    const parkingSpotsRef = db.collection("parkingSpots");
     snapshot = await parkingSpotsRef.get().catch(() => {
-      console.log('No matching documents.');
+      console.log("No matching documents.");
     });
     let parkingSpots = [];
     let ctr = 0;
-    await snapshot.forEach(doc => {
+    await snapshot.forEach((doc) => {
       parkingSpots.push(doc.data());
       parkingSpots[ctr].id = doc.id;
       ctr++;
@@ -70,12 +70,12 @@ export default function MapScreen (props) {
 
   const markerClick = (event, spotId) => {
     let tempSpots = parkingSpots;
-    let spot = tempSpots.filter(spot => {
+    let spot = tempSpots.filter((spot) => {
       return spot.id == spotId;
     });
-    props.navigation.navigate('MapSingleSpotScreen', {
+    props.navigation.navigate("MapSingleSpotScreen", {
       parkingSpot: spot[0],
-      user: props.user
+      user: props.user,
     });
   };
 
@@ -83,10 +83,10 @@ export default function MapScreen (props) {
     <SafeAreaView style={styles.safeArea}>
       <View style={styles.searchBar}>
         <GooglePlacesAutocomplete
-          placeholder='Search'
+          placeholder="Search"
           query={{
             key: GOOGLE_API_KEY,
-            language: 'en'
+            language: "en",
           }}
           onPress={async (data = null) => {
             let coords = await geocode(data.description);
@@ -95,10 +95,10 @@ export default function MapScreen (props) {
               pitch: 0,
               zoom: 13,
               heading: 0,
-              altitude: 0
+              altitude: 0,
             });
           }}
-          onFail={error => console.error(error)}
+          onFail={(error) => console.error(error)}
         />
       </View>
       <View style={styles.key}>
@@ -116,36 +116,36 @@ export default function MapScreen (props) {
         showsUserLocation
         showsBuildings
         showsMyLocationButton={true}
-        mapType={'mutedStandard'}
+        mapType={"mutedStandard"}
         camera={region}
         initialCamera={region}
-        onCameraChangeComplete={async region => {
+        onCameraChangeComplete={async (region) => {
           let tempRegion = {
             center: {
               latitude: Number(region.latitude).toFixed(4),
-              longitude: Number(region.longitude).toFixed(4)
+              longitude: Number(region.longitude).toFixed(4),
             },
             pitch: 0,
             zoom: 13,
             heading: 0,
-            altitude: 0
+            altitude: 0,
           };
           setRegion(tempRegion);
         }}
       >
-        {parkingSpots?.map(spot => {
+        {parkingSpots?.map((spot) => {
           return (
             <Marker
               key={spot.id}
               coordinate={{
                 latitude: spot.latitude,
-                longitude: spot.longitude
+                longitude: spot.longitude,
               }}
-              pinColor={props.user.id == spot.userId ? 'blue' : 'red'}
+              pinColor={props.user.id == spot.userId ? "#1A659E" : "#ff6b35"}
             >
               <Callout
                 key={spot.id}
-                onPress={event => markerClick(event, spot.id)}
+                onPress={(event) => markerClick(event, spot.id)}
               >
                 <Text>{spot.description}</Text>
               </Callout>
